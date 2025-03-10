78121a891b0017272b2dc65db34ebce7d2c5311b
Kallol Hazarika <Kallol.Hazarika@mu-sigma.com>
2025-03-10 19:49:00 +0530

    added hero

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,21 @@
-import { Geist, Geist_Mono } from "next/font/google";
+// import { Geist, Geist_Mono } from "next/font/google";
+import { Roboto } from 'next/font/google'
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const roboto = Roboto({
+  weight: '400',
+  subsets: ['latin'],
+})
+
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 export const metadata = {
   title: "Create Next App",
@@ -18,8 +24,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+    <html lang="en" className={roboto.className}>
+      <body>
         {children}
       </body>
     </html>
--- app/page.js
+++ app/page.js
@@ -1,17 +1,27 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+// import styles from "./page.module.css";
 import Cell from "./ui/components/cell";
+import styles from "./ui/css/Hero.module.css"
+import calculator from "../public/calculator.jpg"
 
 export default function Home() {
   return (
-    <div>
-      <Cell 
-        color = "black"
-      />
-      
-      <Cell 
-        color = "orange"
-      />
-    </div>
+    <>
+    <section className={styles.hero}>
+      <div className={styles.header}>How Does a Calculator Work?</div>
+      <div className={styles.content}>
+        <div>
+          Ever wondered how a simple clculator actually works? In this turtorial, we'll dive into the world of stack data structure and how this data structure is used in the workings of a calculator.
+        </div>
+        <Image className={styles.image} src = {calculator} alt = "calculator image" width={400}/>
+      </div>
+    </section>
+    <section className="Stack">
+
+    </section>
+    <section className="Calculator">
+
+    </section>
+    </>
   );
 }
