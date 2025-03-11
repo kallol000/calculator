14de99beb345d0eb404b8070ec448f31ebe5b347
Kallol Hazarika <Kallol.Hazarika@mu-sigma.com>
2025-03-11 16:19:29 +0530

    added content for problem statement

--- app/page.js
+++ app/page.js
@@ -3,15 +3,16 @@ import Image from "next/image";
 import Cell from "./ui/components/cell";
 import styles from "./ui/css/Hero.module.css"
 import calculator from "../public/calculator.jpg"
+import { content } from "./lib/content";
 
 export default function Home() {
   return (
     <>
     <section className={styles.hero}>
-      <div className={styles.header}>How Does a Calculator Work?</div>
+      <div className={styles.header}>How Does a Calculator Actually Work?</div>
       <div className={styles.content}>
         <div>
-          Ever wondered how a simple clculator actually works? In this turtorial, we'll dive into the world of stack data structure and how this data structure is used in the workings of a calculator.
+          {content.hero}
         </div>
         <Image className={styles.image} src = {calculator} alt = "calculator image" width={400}/>
       </div>
